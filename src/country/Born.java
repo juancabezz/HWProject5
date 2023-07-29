02a5cfc98c12f6e8d263a12c419da26fd9966db4
juancabeza <juancabeza@localhost>
2023-07-28 23:16:39 -0400

    Initial Commit

---
+++ src/country/Born.java
@@ -0,0 +1,11 @@
+package country;
+
+public class Born {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("I was born and raised in Colombia...");
+		System.out.println("I was born in Bogota, the capital of Colombia...");
+	}
+
+}
